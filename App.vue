6678f72b40804f25ba14c0ca3fb3e538a793fe27
andresnar <49461419+andresnar@users.noreply.github.com>
2019-06-05 10:18:03 -0500

    Add files via upload
    Ya están corregidos los containers y todo se centra en la pagina excepto el video jajajajaja

--- App.vue
+++ App.vue
@@ -1,47 +1,41 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span class="font-weight-light">CUIDA A TUS FAMILIARES CON ALZHEIMER</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://www.alz.org/"
-        target="_blank"
-      >
-        <span class="mr-2">Mas Informacion</span>
-      </v-btn>
-    </v-toolbar>
-    
+    <v-layout align-space-around justify-center column fill-height>
+      <v-flex>
+        <v-toolbar app>
+          <v-toolbar-title class="headline text-uppercase">
+            <span class="font-weight-light">CUIDA A TUS FAMILIARES CON ALZHEIMER</span>
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn flat href="https://www.alz.org/" target="_blank">
+            <span class="mr-2">Mas Informacion</span>
+          </v-btn>
+          </v-toolbar>
+      </v-flex>
+      
+      <v-content>
+        <barra/>
+        <hr>
+        <Carrusel/>
+      </v-content>
 
-    <v-content>
-      <barra/>
-      <hr>
-    </v-content>
+      <v-content align-space-around>
+        <h1 id="titulo1" align="center"><u>Amar Sin Condición</u></h1>
+        <v-tabs v-model="active" color="cyan" dark slider-color="yellow">
+          <v-tab ></v-tab>
+        </v-tabs>
+        <iframe width="1150" height="550" src="https://www.youtube.com/embed/UkviIKg0-5k" 
+          frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; 
+          picture-in-picture" allowfullscreen>
+        </iframe>
+      </v-content>
 
+      <v-content>
+        <pagina/>
+        <Footer/>
+      </v-content>
 
-    <v-conteiner aling="center">
-      <v-layout test-xs-center wrap>
-      <Carrusel/><br>
-      </v-layout>
-      <h1 id="titulo1" align="center"><u>Amar Sin Condición</u></h1>
-
-      <v-tabs v-model="active" color="cyan" dark slider-color="yellow">
-        <v-tab ></v-tab>
-      </v-tabs>
-      <v-layout text-xs-center wrap>  
-      <iframe width="1150" height="550" src="https://www.youtube.com/embed/UkviIKg0-5k" frameborder="0" 
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-      </iframe>
-      </v-layout>
-    </v-conteiner>
-
-    <v-content>
-      <pagina/>
-      <Footer/>
-    </v-content>
-    
+    </v-layout>    
   </v-app>
 </template>
 
--- Pagina.vue
+++ Pagina.vue
@@ -1,84 +1,68 @@
 <template>
-    <v-container aria-expanded="center">
-        <hr>
-        <div id="text1">
+    <v-layout align-center justify-start column >
+        <v-flex align-center>
+            <hr>
             <h1 align="center"><b>Estadisticas</b></h1>
-            <h3>
-            Cada 66 segundos, alguien en los Estados Unidos de América desarrolla el Alzheimer. Se estima que cerca de 500.000 nuevos 
-            casos de la enfermedad de Alzheimer serán diagnosticados este año. Entérese de los datos reales sobre la enfermedad de 
-            Alzheimer, la forma más común de demencia. <br><br>
-            El Alzheimer no es una parte normal del envejecimiento.
-            La enfermedad de Alzheimer es una degeneración irreversible del cerebro que causa trastornos en la memoria, la cognición, la personalidad 
-            y otras funciones que eventualmente conducen a la muerte debido a un cese completo cerebral.
-            </h3><br>
+            <p id="texto">
+                Cada 66 segundos, alguien en los Estados Unidos de América desarrolla el Alzheimer.
+                Se estima que cerca de 500.000 nuevos casos de la enfermedad de Alzheimer serán 
+                diagnosticados este año. Entérese de los datos reales sobre la enfermedad de 
+                Alzheimer, la forma más común de demencia. <br><br>
+                El Alzheimer no es una parte normal del envejecimiento. La enfermedad de 
+                Alzheimer es una degeneración irreversible del cerebro que causa trastornos 
+                en la memoria, la cognición, la personalidad y otras funciones que eventualmente
+                 conducen a la muerte debido a un cese completo cerebral.
+                </p> <br>
+        </v-flex>
+        <v-container class="container" align-space-around justify-space-around>
+                <div id="minicajitas">MÁS DE 16 MILLONES DE ESTADOUNIDENSES BRINDAN ATENCIÓN NO REMUNERADA A PERSONAS CON ALZHEIMER U OTRAS DEMENCIAS.</div>
+                <div id="minicajitas">LA ENFERMEDAD DE ALZHEIMER ES LA 6ª causa principal de muerte Estados Unidos.</div>
+                <div id="minicajitas">Entre 2000 y 2017, las muertes debidas a enfermedades cardíacas han disminuido un 19% mientras que las muertes debidas a la enfermedad de Alzheimer han aumentado un 145%.</div>
+                <div id="minicajitas">1 DE CADA 3 adultos mayores muere con alzhéimer u otro tipo de demencia. Ocasiona la muerte a más personas que el cáncer de mama y el cáncer de próstata COMBINADOS.</div>
+        </v-container >
 
-            <v-container class="container" align="center">
-                <div aling="center" id="minicajitas">MÁS DE 16 MILLONES DE ESTADOUNIDENSES BRINDAN ATENCIÓN NO REMUNERADA A PERSONAS CON ALZHEIMER U OTRAS DEMENCIAS.</div>
-                <div aling="center" id="minicajitas">LA ENFERMEDAD DE ALZHEIMER ES LA 6ª causa principal de muerte Estados Unidos.</div>
-                <div aling="center" id="minicajitas">Entre 2000 y 2017, las muertes debidas a enfermedades cardíacas han disminuido un 19% mientras que las muertes debidas a la enfermedad de Alzheimer han aumentado un 145%.</div>
-                <div aling="center" id="minicajitas">1 DE CADA 3 adultos mayores muere con alzhéimer u otro tipo de demencia. Ocasiona la muerte a más personas que el cáncer de mama y el cáncer de próstata COMBINADOS.</div>
-            </v-container >
-
-            <v-conteiner class="container">
-                <div><img src="https://www.neurologia.com/img/imagenes/2017519_2017519_02.jpg" height="500" with="300"></div>
-                <div><img src="https://www.neurologia.com/img/imagenes/2017519_2017519_01.jpg" height="600" with="500"></div>
-            </v-conteiner>
-
+        <v-container class="container" fill-height justify-space-around>
+                <div><img src="https://www.neurologia.com/img/imagenes/2017519_2017519_02.jpg" height="300" with="200"></div>
+                <div><img src="https://www.neurologia.com/img/imagenes/2017519_2017519_01.jpg" height="300" with="300"></div>
+        </v-container>
+        
+        <v-conteiner class="conteiner">
             <h1 align="center">Especificaciones</h1>
-            <h3 align="left">✅ Localizador + Comunicador SOS móvil (para tarjeta SIM). Listo para usar inmediatamente: Enchufar y listo. Nuestro modelo incorpora una versión mejorada de su "firmware" para que detecte automáticamente la tarjeta SIM incluida -y la mayoría de las tarjetas SIM en España- y la conecte a GPRS. Te garantizamos que te funcionará en menos de 3 minutos (tras activar la tarjeta, desactivar la petición de PIN e introducirla correctamente en el dispositivo ya "cargado"). Si no te funcionase, contáctanos <br>
+            <p align="left" id="texto">✅ Localizador + Comunicador SOS móvil (para tarjeta SIM). Listo para usar inmediatamente: Enchufar y listo. Nuestro modelo incorpora una versión mejorada de su "firmware" para que detecte automáticamente la tarjeta SIM incluida -y la mayoría de las tarjetas SIM- y la conecte a GPRS. Te garantizamos que te funcionará en menos de 3 minutos (tras activar la tarjeta, desactivar la petición de PIN e introducirla correctamente en el dispositivo ya "cargado"). Si no te funcionase, contáctanos <br>
                 ✅ A través de esta alarma personal podrás localizar mediante GPS y AGPS/LBS + WiFi, funciona tanto en exteriores como en interiores siempre que tenga cobertura de móvil. Podrás consultar la localización del dispositivo mediante APP (en castellano) o a través de tu PC (navegador). <br>
                 ✅ El localizador gps tiene una función de comunicación dual o de doble vía: podrás hablar y escuchar a través del dispositivo (función manos libres).<br>
                 ✅ Este comunicador personal cuenta con batería interna recargable de litio con una autonomía de hasta 72-300 horas (según tarjeta SIM usada y configuración).<br>
-                ✅ ¡ Nuevo modelo a la venta ! Incluye una oferta en la que recibirás de forma gratuita una tarjeta SIM prepago con un saldo inicial de 5 euros (tras activación) + Correa tipo "reloj" para el localizador-comunicador. Manual y Soporte en Castellano. IMPORTANTE: no es obligado usar nuestra tarjeta pero si MUY RECOMENDABLE por coste y, sobre todo, porque MAXIMIZA la duración de la batería llegando a multiplicar por 4 o 5 la duración respecto a otras SIM (ver condiciones/tarifas SIM en producto)<br>
-            </h3>
-            <br>
-            <v-container class="conteiner">
-                <img src="https://images-na.ssl-images-amazon.com/images/I/61%2BDWlktRiL._SL1000_.jpg" height="600" with="600">
-                <img src="https://images-na.ssl-images-amazon.com/images/I/511FWYjtMSL.jpg" height="600" with="600">
-                <img src="https://images-na.ssl-images-amazon.com/images/I/4167m-1zapL.jpg" height="600" with="600">
+                ✅ ¡ Nuevo modelo a la venta ! Incluye una oferta en la que recibirás de forma gratuita una tarjeta SIM prepago con un saldo inicial de $15.000 (tras activación) + Correa tipo "reloj" para el localizador-comunicador. Manual y Soporte en Castellano. IMPORTANTE: no es obligado usar nuestra tarjeta pero si MUY RECOMENDABLE por coste y, sobre todo, porque MAXIMIZA la duración de la batería llegando a multiplicar por 4 o 5 la duración respecto a otras SIM (ver condiciones/tarifas SIM en producto)<br>
+            </p><br>
+        </v-conteiner>
+
+        <v-container align-center justify-space-around row fill-with>
+                <img src="https://images-na.ssl-images-amazon.com/images/I/61%2BDWlktRiL._SL1000_.jpg" height="300" with="300">
+                <img src="https://images-na.ssl-images-amazon.com/images/I/511FWYjtMSL.jpg" height="300" with="300">
+                <img src="https://images-na.ssl-images-amazon.com/images/I/4167m-1zapL.jpg" height="300" with="300">
                 
             </v-container>
-        </div>
-    </v-container>
+
+    </v-layout>
 </template>
 
-<script>
-export default {
-
-}
-</script>
-
 <style>
-    
-    #pag{
-        color: rgba(21, 27, 78, 0.911);
-        background-color: lightgray;
-        font-size:30px;
-        text-align: center;
-        width: 1000px;
-        border: 13px solid lightblue;
-        padding: 1rem;
-        margin: 20px;
-        text-shadow: 3px 2px lightgray;
-    }
-    
     #minicajitas{
-        border: 10px solid black;
         background-color: lightblue;
         font-size:20px;
         text-align: center;
         width: 1000px;
-        border: 13px solid lightblue;
+        border: 1px solid lightseagreen;
         padding: 1rem;
-        margin: 20px;
+        margin: 10px;
         text-shadow: 3px 2px lightgray;
     }
     .container {
         margin: 0 0 1rem;
         display: flex;
-        align-items: start;
+        
     }
-    
-
-
+    #texto{
+        font-size:18px;
+    }
 </style>
